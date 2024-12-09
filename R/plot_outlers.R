2495a2a5b28c15b23261555b5ac0b662404de8a4
yusufmian2 <113137846+yusufmian2@users.noreply.github.com>
2024-12-09 17:17:08 -0500

    plot quote fix

--- R/plot_outlers.R
+++ R/plot_outlers.R
@@ -13,24 +13,30 @@ plot_outliers <- function(data, variable) {
   variable <- enquo(variable)
 
   column_name <- as_label(variable)
+
   if (!column_name %in% names(data)) {
     stop(paste("Variable", column_name, "not found in the dataset."))
   }
 
-  outliers <- boxplot.stats(data[[column_name]])$out
-  outlier_data <- data %>%
-    filter(!!variable %in% outliers)
+  if (all(is.na(data[[column_name]]))) {
+    stop(paste("Variable", column_name, "contains only NA values."))
+  }
 
+  outliers <- boxplot.stats(data[[column_name]])$out
+  outlier_data <- data %>% filter(!!variable %in% outliers)
+  t
   ggplot(data, aes(y = !!variable, x = 1)) +
     geom_boxplot(outlier.colour = "red", outlier.size = 3, fill = "lightblue") +
-    geom_point(
-      data = outlier_data,
-      aes(y = !!variable, x = 1),
-      color = "red",
-      size = 3,
-      shape = 21,
-      stroke = 1.5
-    ) +
+    if (length(outliers) > 0) {
+      geom_point(
+        data = outlier_data,
+        aes(y = !!variable, x = 1),
+        color = "red",
+        size = 3,
+        shape = 21,
+        stroke = 1.5
+      )
+    } +
     labs(
       title = paste("Boxplot of", column_name),
       y = column_name,
