8e0850a280a69858cad1c3d3ca961edd241e050d
yusufmian2 <113137846+yusufmian2@users.noreply.github.com>
2024-12-09 17:04:25 -0500

    plot fix 2

--- R/plot_outlers.R
+++ R/plot_outlers.R
@@ -10,24 +10,22 @@ plot_outliers <- function(data, variable) {
   library(ggplot2)
   library(dplyr)
 
-  variable <- enquo(variable)  # Capture the bare column name
+  variable <- enquo(variable)
 
-  # Check if the column exists
   column_name <- as_label(variable)
   if (!column_name %in% names(data)) {
     stop(paste("Variable", column_name, "not found in the dataset."))
   }
 
-  # Identify outliers
   outliers <- boxplot.stats(data[[column_name]])$out
-  outlier_data <- data %>% filter(!!variable %in% outliers)
+  outlier_data <- data %>%
+    filter(!!variable %in% outliers)
 
-  # Create the plot
-  ggplot(data, aes(y = !!variable)) +
+  ggplot(data, aes(y = !!variable, x = 1)) +
     geom_boxplot(outlier.colour = "red", outlier.size = 3, fill = "lightblue") +
     geom_point(
       data = outlier_data,
-      aes(y = !!variable),
+      aes(y = !!variable, x = 1),
       color = "red",
       size = 3,
       shape = 21,
