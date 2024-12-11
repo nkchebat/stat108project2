3890edc6576e9e2e4b7931a1083ae543f2e28f7f
yusufmian2 <113137846+yusufmian2@users.noreply.github.com>
2024-12-11 18:46:55 -0500

    data description

---
+++ data/data_birth.R
@@ -0,0 +1,14 @@
+#' data_birth
+#'
+#' A dataset containing example information from a mice experiment
+#'  The variables are as follows:
+#'
+#' @format A data frame with 32 rows and 4 variables:
+#' \describe{
+#'   \item{ID}{identification of a mouse}
+#'   \item{Sex}{predicted Sex each mice}
+#'   \item{Num}{assigned number to each mice}
+#'   \item{Treatment}{Binary indicator of placebo or treatment received}
+#' }
+#'
+#' @source James Xenakis, Harvard University
---
+++ data/data_weight.R
@@ -0,0 +1,17 @@
+#' data_weight
+#'
+#' A dataset containing example weights from a mice experiment
+#'  The variables are as follows:
+#'
+#' @format A data frame with 38 rows and 7 variables:
+#' \describe{
+#'   \item{ID}{identification of a mouse}
+#'   \item{Body.Weight.1}{initial measurement of each mice}
+#'   \item{Date.Body.Weight.1}{the date of the first measurement}
+#'   \item{Body.Weight.2}{the second weight measurement}
+#'   \item{Date.Body.Weight.2 }{date of the second measurement}
+#'   \item{Body.Weight.3 }{the third weight measurement}
+#'   \item{Date.Body.Weight.2.1 }{date of the third measurement}
+#' }
+#'
+#' @source James Xenakis, Harvard University
